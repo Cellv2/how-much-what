a197f6605fdfe46d77dd1b5e8ceaf1484783666b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2026-01-06 22:07:33 +0000

    Green test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,7 @@
-export const Header = () => {
-    return <div>Header</div>;
+export type HeaderProps = {
+    title: string;
+};
+
+export const Header = (props: HeaderProps) => {
+    return <h1>{props.title}</h1>;
 };
