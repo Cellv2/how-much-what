0f56b55941519111b0ad84d68a3b1b2c6c90202d
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2026-01-20 20:27:07 +0000

    Add active onclick and aria attributes

--- src/components/GearMenu/GearMenu.test.tsx
+++ src/components/GearMenu/GearMenu.test.tsx
@@ -1,7 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { composeStories } from "@storybook/react-vite";
+import { fireEvent, render, screen, within } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { getGearSlotNames } from "../../models/gearSlot";
+import {
+    DEFAULT_GEAR_SLOT,
+    GEAR_SLOT,
+    getGearSlotNames,
+} from "../../models/gearSlot";
+import { expectToBeDefined } from "../../tests/assertions";
 import { GearMenu } from "./GearMenu";
+import * as stories from "./GearMenu.stories";
+
+const { DefaultGearMenu } = composeStories(stories);
 
 describe("[Behaviour] GearMenu component", () => {
     it("should render each expected gear slot", () => {
@@ -30,17 +39,73 @@ describe("[Behaviour] GearMenu component", () => {
         expect(actual).toEqual(expected);
     });
 
-    it("should display gear slot labels with the correct text", () => {});
+    it("should have the correct default slot set to active on render", () => {
+        render(<GearMenu />);
 
-    it("should highlight the correct default slot", () => {});
+        const defaultSlotLabel = GEAR_SLOT[DEFAULT_GEAR_SLOT].name;
+
+        const list = screen.getByRole("list", { name: "gearSlot-selector" });
+        const defaultSlot = within(list).getByText(defaultSlotLabel);
+
+        expect(list).toBeInTheDocument();
+        expect(defaultSlot).toHaveAttribute("aria-selected", "true");
+    });
+
+    it("should activate the correct gear slot when clicked", () => {
+        render(<GearMenu />);
+
+        const list = screen.getByRole("list", { name: "gearSlot-selector" });
+        const helm = within(list).getByText(GEAR_SLOT.HELM.name);
+        const shoulder = within(list).getByText(GEAR_SLOT.SHOULDER.name);
+
+        expectToBeDefined(helm, "Helm slot");
+        expectToBeDefined(shoulder, "Shoulder slot");
+
+        fireEvent.click(helm);
+        expect(helm).toHaveAttribute("aria-selected", "true");
+        expect(shoulder).toHaveAttribute("aria-selected", "false");
+
+        fireEvent.click(shoulder);
+        expect(helm).toHaveAttribute("aria-selected", "false");
+        expect(shoulder).toHaveAttribute("aria-selected", "true");
+    });
 });
 
 describe("[Smoke] GearMenu stories", () => {
-    it("should render the component without crashing", () => {});
+    it("should render the component without crashing", () => {
+        render(<DefaultGearMenu />);
 
-    it("should not render any unexpected gear slots", () => {});
+        const list = screen.getByRole("list", { name: "gearSlot-selector" });
 
-    it("should render with default props", () => {});
+        expect(list).toBeInTheDocument();
+    });
+
+    it("should not render any unexpected gear slots", () => {
+        render(<DefaultGearMenu />);
+
+        const list = screen.getByRole("list", { name: "gearSlot-selector" });
+
+        const expectedNames = getGearSlotNames();
+        const renderedItems = within(list).getAllByRole("listitem");
+
+        expect(renderedItems).toHaveLength(expectedNames.length);
+        for (const name of expectedNames) {
+            const expected = within(list).getByText(name);
+            expect(expected).toBeInTheDocument();
+        }
+    });
+
+    it("should render with internal defaults", () => {
+        render(<DefaultGearMenu />);
+
+        const defaultSlotLabel = GEAR_SLOT[DEFAULT_GEAR_SLOT].name;
+
+        const element = screen.getByRole("list", { name: "gearSlot-selector" });
+        const defaultSlot = within(element).getByText(defaultSlotLabel);
+
+        expect(element).toBeInTheDocument();
+        expect(defaultSlot).toHaveAttribute("aria-selected", "true");
+    });
 });
 
 // it.skip("should update the costs when an item is selected", () => {})
@@ -49,3 +114,4 @@ describe("[Smoke] GearMenu stories", () => {
 // it("should make the function call to update the available item variants when selecting a slot", () => {});
 // it("should show item variants when a gear slot is clicked", () => {});
 // it("should highlight the slot when selected", () => {});
+// it("should have the correct aria role", () => {});
--- src/components/GearMenu/GearMenu.tsx
+++ src/components/GearMenu/GearMenu.tsx
@@ -1,11 +1,27 @@
-import { GEAR_SLOT, getGearSlotIds } from "../../models/gearSlot";
+import { useState } from "react";
+import {
+    DEFAULT_GEAR_SLOT,
+    GEAR_SLOT,
+    getGearSlotIds,
+    type GearSlotId,
+} from "../../models/gearSlot";
 
 export const GearMenu = () => {
+    const [activeGearSlot, setActiveGearSlot] =
+        useState<GearSlotId>(DEFAULT_GEAR_SLOT);
+
     const slotIds = getGearSlotIds();
     const menuSlots = slotIds.map((slotId) => {
         const slotLabel = GEAR_SLOT[slotId].name;
         return (
-            <li role="listitem" key={slotId}>
+            <li
+                aria-selected={slotId === activeGearSlot}
+                role="listitem"
+                key={slotId}
+                onClick={() => {
+                    setActiveGearSlot(slotId);
+                }}
+            >
                 {slotLabel}
             </li>
         );
